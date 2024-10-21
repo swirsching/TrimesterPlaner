be695949014742c9c0831cfc9fcf5387eb940ef7
Steffen Wirsching <WST@BPC21147>
2024-10-21 13:57:40 +0200

    Allow changing developer of a plan.

--- TrimesterPlaner/ViewModels/PlanViewModel.cs
+++ TrimesterPlaner/ViewModels/PlanViewModel.cs
@@ -4,9 +4,31 @@ using TrimesterPlaner.Utilities;
 
 namespace TrimesterPlaner.ViewModels
 {
-    public class PlanViewModel(Plan plan, IPlanManager planManager, IEntwicklungsplanManager entwicklungsplanManager) : BaseViewModel(entwicklungsplanManager)
+    public class PlanViewModel(Plan plan, IPlanManager planManager, IDeveloperProvider developerProvider, IEntwicklungsplanManager entwicklungsplanManager) : BaseViewModel(entwicklungsplanManager)
     {
-        public Plan Plan { get => plan; }
+        public Plan Plan { get; } = plan;
+
+        private bool _IsChangingDeveloper = false;
+        public bool IsChangingDeveloper
+        {
+            get => _IsChangingDeveloper;
+            set => SetProperty(ref _IsChangingDeveloper, value);
+        }
+
+        public IEnumerable<Developer> Developers { get; } = developerProvider.GetDevelopers();
+
+        public Developer? SelectedDeveloper
+        {
+            get => Plan.Developer;
+            set
+            {
+                if (value is not null)
+                {
+                    Plan.Developer = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public ICommand MoveUpCommand { get; } = new RelayCommand((o) => planManager.MoveUp(plan));
         public ICommand MoveDownCommand { get; } = new RelayCommand((o) => planManager.MoveDown(plan));
