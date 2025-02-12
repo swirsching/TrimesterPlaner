566de05489aa8eab7a76bf80cae282f13c8e6eec
Steffen Wirsching <WST@BPC21147>
2025-02-12 15:12:29 +0100

    Add InverseBoolToVisibilityConverter.

---
+++ TrimesterPlaner/Converters/InserveBoolToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TrimesterPlaner.Converters
+{
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b && !b)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
