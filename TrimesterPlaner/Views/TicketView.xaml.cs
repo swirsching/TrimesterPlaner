af2d1e407deac424a636f6736ae06b8fbca8b99d
Steffen Wirsching <WST@BPC21147>
2025-03-25 16:51:11 +0100

    Use gong solutions rather than implementing drag-and-drop on my own.

--- TrimesterPlaner/Views/TicketView.xaml.cs
+++ TrimesterPlaner/Views/TicketView.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using TrimesterPlaner.Extensions;
-using TrimesterPlaner.Models;
-using TrimesterPlaner.ViewModels;
+﻿using System.Windows.Controls;
 
 namespace TrimesterPlaner.Views
 {
@@ -13,27 +7,6 @@ namespace TrimesterPlaner.Views
         public TicketView()
         {
             InitializeComponent();
-        }
-
-        private void Ticket_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var fe = (FrameworkElement)sender;
-            var container = fe.FindAncestor<ItemsControl>();
-            var tickets = (ObservableCollection<Ticket>)((TicketProviderViewModel)container!.DataContext).Tickets;
-            var ticket = ((TicketViewModel)fe.DataContext).Ticket;
-
-            tickets.Remove(ticket);
-            DragDrop.DoDragDrop(fe, new DataObject(ticket), DragDropEffects.Move);
-        }
-
-        private void Ticket_Drop(object sender, DragEventArgs e)
-        {
-            var fe = (FrameworkElement)sender;
-            var container = fe.FindAncestor<ItemsControl>();
-            var tickets = (ObservableCollection<Ticket>)((TicketProviderViewModel)container!.DataContext).Tickets;
-            var ticket = ((TicketViewModel)fe.DataContext).Ticket;
-
-            tickets.Insert(tickets.IndexOf(ticket), (Ticket)e.Data.GetData(typeof(Ticket)));
         }
     }
 }
